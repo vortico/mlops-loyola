ef5894c903b40b8f621858c76ecc5596b4dc4687
miguel duran <migduroli@gmail.com>
2025-01-17 00:32:41 +0100

    :sparkles: Add mlflow

--- airflow/dags/train_pipeline.py
+++ airflow/dags/train_pipeline.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 import pandas as pd
 from airflow.operators.python import PythonOperator
+from mlflow.pyfunc import PythonModel
 from sklearn.model_selection import train_test_split
 
+import mlflow
 from airflow import DAG
 from mlops.config import MODEL_CONFIG
 from mlops.processors.churn import ChurnProcessor
@@ -23,16 +25,27 @@ default_args = {
 
 DATA_PATH = Path("./data/churn/data.parquet")
 MODEL_PATH = Path("./artifacts/models/churn/model.flm")
+MLFLOW_URI = "http://mlflow:5001"
+
+mlflow.set_tracking_uri(MLFLOW_URI)
+
+
+class ChurnPredictionModel(PythonModel):
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, context, model_input, params=None):  # type: ignore
+        return self.model.predict(model_input)
 
 
 def load_data(**context):
-    """Load the dataset and split it into train and test sets"""
+    # Load the dataset
     dataset = pd.read_parquet(DATA_PATH)
     logger.info("Read parquet from: %s", DATA_PATH.as_posix())
 
+    # Split the dataset into train and test sets
     X = dataset.drop(columns=[MODEL_CONFIG["models"]["churn"]["target"]])
     y = dataset[MODEL_CONFIG["models"]["churn"]["target"]]
-    logger.info("X, y data loaded")
 
     X_train, X_test, y_train, y_test = train_test_split(
         X,
@@ -42,6 +55,7 @@ def load_data(**context):
     )
     logger.info("Tran-test split done")
 
+    # Push data to XCom
     logger.info("Pushing data to XCom")
     context["task_instance"].xcom_push(key="X_train", value=X_train.to_dict())  # type: ignore
     context["task_instance"].xcom_push(key="X_test", value=X_test.to_dict())  # type: ignore
@@ -50,7 +64,7 @@ def load_data(**context):
 
 
 def train_model(**context):
-    """Train the churn model"""
+    # Pull data from XCom
     task_instance = context["task_instance"]
     X_train = pd.DataFrame(task_instance.xcom_pull(key="X_train"))
     y_train = pd.Series(task_instance.xcom_pull(key="y_train"))
@@ -90,7 +104,52 @@ def evaluate_model(**context):
     logger.info("Metrics pushed to XCom")
 
 
-# Create the DAG
+def mlflow_register_model(**context):
+    # Pull metrics from XCom
+    task_instance = context["task_instance"]
+    metrics = task_instance.xcom_pull(key="metrics")
+
+    # Set the experiment
+    mlflow.set_experiment("churn-prediction-loyola")
+
+    # Load the model
+    model = ChurnProcessor.load(MODEL_PATH.as_posix())
+
+    # Log the model
+    with mlflow.start_run():
+        mlflow.log_params(MODEL_CONFIG["models"]["churn"])
+        mlflow.log_metrics(metrics)
+        mlflow.pyfunc.log_model(
+            "model",
+            python_model=ChurnPredictionModel(model),
+            artifacts={"model": MODEL_PATH.as_posix()},
+        )
+        run_id = mlflow.active_run().info.run_id
+
+    # Push run_id to XCom
+    context["task_instance"].xcom_push(key="run_id", value=run_id)
+    logger.info("Run ID pushed to XCom: %s", run_id)
+
+
+def mlflow_load_model(**context):
+    # Pull test data from XCom
+    task_instance = context["task_instance"]
+    run_id = task_instance.xcom_pull(key="run_id")
+
+    # Load the model
+    model = mlflow.pyfunc.load_model(f"runs:/{run_id}/model")
+
+    # Make predictions
+    X_test = pd.DataFrame(task_instance.xcom_pull(key="X_test")).values
+    predictions = model.predict(X_test)
+    logger.info("Predictions: %s", predictions)
+
+    # Push predictions to XCom
+    context["task_instance"].xcom_push(key="predictions", value=predictions[0].tolist())
+    context["task_instance"].xcom_push(key="scores", value=predictions[1].tolist())
+    logger.info("Predictions pushed to XCom")
+
+
 with DAG(
     "churn_training_pipeline",
     default_args=default_args,
@@ -118,5 +177,15 @@ with DAG(
         python_callable=evaluate_model,
     )
 
+    mlflow_register_model_task = PythonOperator(
+        task_id="mlflow_register_model",
+        python_callable=mlflow_register_model,
+    )
+
+    mlflow_load_model_task = PythonOperator(
+        task_id="mlflow_load_model",
+        python_callable=mlflow_load_model,
+    )
+
     # Define task dependencies
-    load_data_task >> train_model_task >> evaluate_model_task
+    load_data_task >> train_model_task >> evaluate_model_task >> mlflow_register_model_task >> mlflow_load_model_task
